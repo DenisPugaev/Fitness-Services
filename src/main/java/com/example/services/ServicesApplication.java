584618952127b8822664a0c0de407c234f2491c8
DeN <den.denis.denis@mail.ru>
2023-07-04 22:06:15 +0300

    1

--- src/main/java/com/example/services/ServicesApplication.java
+++ src/main/java/com/example/services/ServicesApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServicesApplication {
 
 
+
     public static void main(String[] args) {
         SpringApplication.run(ServicesApplication.class, args);
     }
